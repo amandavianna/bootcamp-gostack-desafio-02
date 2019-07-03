d9dfb9e5f02b9e60bf4326c4414f1c0f2c8c853a
Amanda Vianna <amanda_vianna@hotmail.com>
2019-07-02 23:34:54 -0300

    Gerando hash da senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { hash } from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,10 +7,17 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await hash(user.password, 8);
+      }
+    });
   }
 }
 
