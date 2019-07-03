c258d0745a9ae9942e03d4a59470fae64c745add
Amanda Vianna <amanda_vianna@hotmail.com>
2019-07-02 22:58:04 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Desafio2' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Amanda',
+    email: 'amanda_vianna@hotmail.com',
+    password_hash: '12345',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
